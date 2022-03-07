ee66614993fd423506ef4170f3434010296f9ae2
user <menmenwan46@gmail.com>
2022-03-07 12:06:01 +0800

    完成

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,14 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string text = "text";
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            var w =(meal_cost+ tip_percent +tax_percent);
+            Console.WriteLine("總餐費："+w);
+
         }
+
     }
 }
